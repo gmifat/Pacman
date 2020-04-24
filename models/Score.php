7ee64277d38a53034d4b993eb86930280812be8d
Gmiden <gmiden.fatma@gmail.com>
2020-04-24 21:55:44 +0200

    remove try catch

--- models/Score.php
+++ models/Score.php
@@ -2,68 +2,52 @@
 
     function GetUserMaxScore($user_id)
     {
-        try
-        {
-            $db = dbConnect();
-            $sth = $db->prepare("SELECT MAX(value) FROM scores WHERE id_user = :id");
-            if ($sth->execute([':id' => $user_id])) {
 
-                $result = $sth->fetchColumn();
-                return $result;
-            }
+        $db = dbConnect();
+        $sth = $db->prepare("SELECT MAX(value) FROM scores WHERE id_user = :id");
+        if ($sth->execute([':id' => $user_id])) {
 
-            return '-';
+            $result = $sth->fetchColumn();
+            return $result;
         }
-        catch (Exception $e)
-        {
-            return '-';
-        }
+
+        return '-';
+
     }
 
     function GetTopScores()
     {
-        try
-        {
-            $db = dbConnect();
-            $sth = $db->prepare("SELECT users.username, scores.value FROM scores JOIN users on scores.id_user = users.id ORDER BY value DESC LIMIT 10");
-            if ($sth->execute())
-            {
-                $result = $sth->fetchAll();
-                $count = $sth->rowCount();
-                for ($i = 1; $i <= $count; $i++)
-                {
-                    $result[$i - 1]['rank'] = $i;
-                }
 
-                return $result;
+        $db = dbConnect();
+        $sth = $db->prepare("SELECT users.username, scores.value FROM scores JOIN users on scores.id_user = users.id ORDER BY value DESC LIMIT 10");
+        if ($sth->execute())
+        {
+            $result = $sth->fetchAll();
+            $count = $sth->rowCount();
+            for ($i = 1; $i <= $count; $i++)
+            {
+                $result[$i - 1]['rank'] = $i;
             }
 
-            return false;
+            return $result;
         }
-        catch (Exception $e)
-        {
-            return false;
-        }
+
+        return false;
     }
 
     function AddScore($user_id, $score)
     {
-        try
-        {
-            $db = dbConnect();
-            $sth = $db->prepare("INSERT INTO scores(id_user, value) VALUES(:id_user, :value)");
-            if ($sth->execute([
-                ':id_user' => $user_id,
-                ':value' => $score
-            ]))
-            {
-                return true;
-            }
 
-            return false;
-        }
-        catch (Exception $e)
+        $db = dbConnect();
+        $sth = $db->prepare("INSERT INTO scores(id_user, value) VALUES(:id_user, :value)");
+        if ($sth->execute([
+            ':id_user' => $user_id,
+            ':value' => $score
+        ]))
         {
-            return false;
+            return true;
         }
+
+        return false;
+
     }
--- models/User.php
+++ models/User.php
@@ -2,95 +2,80 @@
 
     function AddUser($username, $email, $password)
     {
-        try
+        $db = dbConnect();
+        $query = $db->prepare("SELECT COUNT(*) as NbUsers from users WHERE username = :username");
+
+        //address ou username déja utilisé
+        if ($query->execute([':username' => $username]))
         {
-            $db = dbConnect();
-            $query = $db->prepare("SELECT COUNT(*) as NbUsers from users WHERE username = :username");
-
-            //address ou username déja utilisé
-            if ($query->execute([':username' => $username]))
+            $count = $query->fetch();
+            $query->closeCursor();
+            if ($count['NbUsers'] > 0)
             {
-                $count = $query->fetch();
-                $query->closeCursor();
-                if ($count['NbUsers'] > 0)
-                {
-                    return "Username existant";
-                }
+                return "Username existant";
             }
-            else
-            {
-                return "une erreur est survenue veuillez réessayer ultérieurement";
-            }
-
-
-            $query = $db->prepare("SELECT COUNT(*) as NbUsers from users WHERE email = :email");
-            if ($query->execute([':email' => $email]))
-            {
-                $count = $query->fetch();
-                $query->closeCursor();
-                if ($count['NbUsers'] > 0)
-                {
-                    return "Email existant";
-                }
-            }
-            else
-            {
-                return "une erreur est survenue veuillez réessayer ultérieurement";
-            }
-
-            // sinon ajouter l'utilisateur
-            $query = $db->prepare("INSERT INTO users (username, email, password) VALUES(:username, :email, :password) ");
-            if($query->execute([
-                ':username' => $username,
-                ':email' => $email,
-                ':password' => password_hash($password, PASSWORD_BCRYPT)
-            ]))
-            {
-               return true;
-            }
-
+        }
+        else
+        {
             return "une erreur est survenue veuillez réessayer ultérieurement";
-
-
         }
-        catch (Exception $e)
+
+        $query = $db->prepare("SELECT COUNT(*) as NbUsers from users WHERE email = :email");
+        if ($query->execute([':email' => $email]))
         {
-            return false;
+            $count = $query->fetch();
+            $query->closeCursor();
+            if ($count['NbUsers'] > 0)
+            {
+                return "Email existant";
+            }
         }
+        else
+        {
+            return "une erreur est survenue veuillez réessayer ultérieurement";
+        }
+
+        // sinon ajouter l'utilisateur
+        $query = $db->prepare("INSERT INTO users (username, email, password) VALUES(:username, :email, :password) ");
+        if($query->execute([
+            ':username' => $username,
+            ':email' => $email,
+            ':password' => password_hash($password, PASSWORD_BCRYPT)
+        ]))
+        {
+            return true;
+        }
+
+        return "une erreur est survenue veuillez réessayer ultérieurement";
+
     }
 
     //vérifier existance utilisateur et son mot de passe
     function FindUser($username, $password)
     {
-        try
-        {
-            $db = dbConnect();
-            $query = $db->prepare("SELECT id, username, email, dateCreate, password from users WHERE   username = :username ");
-            if($query->execute([
-                ':username' => $username
-            ])) {
+        $db = dbConnect();
+        $query = $db->prepare("SELECT id, username, email, dateCreate, password from users WHERE   username = :username ");
+        if($query->execute([
+            ':username' => $username
+        ])) {
 
-                $result = $query->fetch();
+            $result = $query->fetch();
 
-                if ($result === false)
-                {
-                    return false;
-                }
-
-                if (password_verify($password, $result['password']))
-                {
-                    // si l'utilisateur est connu, on récupère son max score s'il y on a
-                    $sth = $db->prepare("SELECT MAX(value) FROM scores WHERE id_user = :id");
-                    $sth->execute([':id' => $result['id']]);
-                    $result['max_score'] = $sth->fetchColumn();
-                    return $result;
-                }
+            if ($result === false)
+            {
+                return false;
             }
 
-            return false;
+            if (password_verify($password, $result['password']))
+            {
+                // si l'utilisateur est connu, on récupère son max score s'il y on a
+                $sth = $db->prepare("SELECT MAX(value) FROM scores WHERE id_user = :id");
+                $sth->execute([':id' => $result['id']]);
+                $result['max_score'] = $sth->fetchColumn();
+                return $result;
+            }
         }
-        catch (Exception $e)
-        {
-            return false;
-        }
+
+        return false;
+
     }
